24698fbd1381f3c71a00838bace7b3310f1f4c2d
kevinelong <kevinelong@gmail.com>
2023-04-25 07:57:11 -0700

    demos

---
+++ conditionals.js
@@ -0,0 +1,17 @@
+const AGE_LIMIT = 16; //CONSTANT FROM THE GOV
+let myAge = 15; // Varies person to person
+let hasPermission = false; // Boolean - true/false
+// let canDrive = myAge >= AGE_LIMIT;
+// console.log( canDrive );
+
+// CONDITIONAL - CONTROL THE FLOW
+if(myAge >= AGE_LIMIT){
+    console.log("YES");
+}else if(hasPermission){
+    console.log("*YES");
+}else{
+    console.log("NO");
+}
+
+console.log("DONE")
+
---
+++ data_types.js
@@ -0,0 +1,15 @@
+
+const firstName = "kevin"; // string data type
+let age = 55; // number data type
+
+console.log(typeof firstName)
+console.log(typeof age)
+
+const letter = "ABC"
+let ascii_value = letter.charCodeAt(0);
+console.log(ascii_value);
+
+ascii_value = ascii_value + 1;//Add one
+console.log(String.fromCharCode(ascii_value))
+
+
