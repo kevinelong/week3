2d51e910a44532e3018b6791be21fef6c536eccb
kevinelong <kevinelong@gmail.com>
2023-04-25 08:51:03 -0700

    demo3

--- Module2/dept_converter.js
+++ Module2/dept_converter.js
@@ -1,14 +1,18 @@
 //switch 
 
-let departmentCode = 5
+let departmentCode = 3
 let departmentName = "default";
 
 switch(departmentCode){
-    case 1: departmentName = "Marketing";break;
-    case 5: departmentName = "Human Resources";break;
-    case 10: departmentName = "Accounting";break;
-    case 12: departmentName = "Legal";break;
-    case 18: departmentName = "IT";break;
-    case 20: departmentName = "Customer Relations";break;
+    case 1: departmentName = "Marketing"; break;
+    case 5: departmentName = "Human Resources"; break;
+    case 10: departmentName = "Accounting"; break;
+    case 12: departmentName = "Legal"; break;
+    case 18: departmentName = "IT"; break;
+    case 20: departmentName = "Customer Relations"; break;
 }
-console.log(departmentCode + " is " + departmentName);
+// CONCATENATION +
+console.log(departmentCode + " is " + departmentName + ".");
+
+//JS TEMPLATE `` ESCAPE VARIABLES with ${v}
+console.log(`${departmentCode} is ${departmentName}.`);
