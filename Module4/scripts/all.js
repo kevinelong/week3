be0a2e32045c2936e119bf5592b9904cf97f8be3
kevinelong <kevinelong@gmail.com>
2023-04-26 06:24:29 -0700

    script demo

---
+++ Module4/scripts/all.js
@@ -0,0 +1,16 @@
+document.body.innerHTML += `
+<nav>
+    <ul>
+        <li>
+            <a href="index.html">
+                Home
+            </a>
+        </li>
+        <li>
+            <a href="products.html">
+                Products
+            </a>
+        </li>
+    </ul>
+</nav>
+`;
