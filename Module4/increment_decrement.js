41ab5745cfcdd10161cc2f5ab55eea1d1afd14ad
kevinelong <kevinelong@gmail.com>
2023-04-26 08:48:23 -0700

    increment decrement concatenate after

---
+++ Module4/increment_decrement.js
@@ -0,0 +1,13 @@
+
+let x = 100;
+x--; //decrement operator - short for: x = x - 1
+console.log(x); //99?
+x++; //increment operator - short for: x = x + 1
+x++; //increment operator - short for: x = x + 1
+console.log(x); //101?
+x += 200; // short for: x = x + 10
+console.log(x); //301?
+////////////////////////////
+text = "ABC"; // set/replace with a single =
+text += "DEF"; //short for text = text + "DEF"
+console.log(text);
